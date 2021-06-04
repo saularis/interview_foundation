aaed7e69710ee2951f023ad5008feebf202d6056
Abdulla Malik <abdulla.coder@gmail.com>
2021-06-05 01:45:42 +0600

    github instance of user

--- app/Services/Github.php
+++ app/Services/Github.php
@@ -2,8 +2,22 @@
 
 namespace App\Services;
 
+use App\User;
+
 class Github
 {
+    protected User $user;
+
+    public function __construct(User $user)
+    {
+        $this->user = $user;
+    }
+
+    public function getUser()
+    {
+        return $this->user;
+    }
+
     public function token()
     {
         return 'sample_token';
--- tests/Unit/GithubTest.php
+++ tests/Unit/GithubTest.php
@@ -2,15 +2,23 @@
 
 namespace Tests\Unit;
 
+use App\User;
 use App\Services\Github;
 use PHPUnit\Framework\TestCase;
 
 class GithubTest extends TestCase
 {
     /** @test */
+    public function it_has_user_instance()
+    {
+        $github = new Github(new User);
+        $this->assertInstanceOf(User::class, $github->getUser());
+    }
+
+    /** @test */
     function it_contains_github_token()
     {
-        $github = new Github;
+        $github = new Github(new User);
         $github->token();
         $this->assertEquals('sample_token', $github->token());
     }
