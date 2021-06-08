203cd1e6cb11d84aee80fa303c72798967956e3e
Abdulla Malik <abdulla.coder@gmail.com>
2021-06-09 02:35:45 +0600

    remove starredrepo component

--- resources/js/components.js
+++ resources/js/components.js
@@ -1,7 +1,5 @@
 import Vue from 'vue';
 import Home from './pages/home.vue';
-import StarredRepo from './pages/StarredRepo.vue'
 
 
 Vue.component('home', Home);
-Vue.component('starred-repo', StarredRepo);
--- resources/js/pages/StarredRepo.vue
+++
@@ -1,3 +0,0 @@
-<template>
-    <h1>Starred Repo</h1>
-</template>
