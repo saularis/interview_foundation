2da588511affa2b01a54fc85b9fb1764de280813
Abdulla Malik <abdulla.coder@gmail.com>
2021-06-09 02:31:44 +0600

    guest landing page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,12 +1,23 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Github Starred Repo</title>
-        <link rel="stylesheet" href="css/app.css">
-    </head>
-    <body>
-        <a href="https://github.com/login/oauth/authorize?client_id=d61f5d673d76ec2ad68a">Github Login</a>
-    </body>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Github Starred Repo</title>
+    <link rel="stylesheet" href="css/app.css">
+</head>
+
+<body>
+    <div class="container h-100">
+        <div class="d-flex justify-content-center mt-5">
+            <a href="/login" class="btn btn-dark">
+                <img class="img-responsive" height="28px" src="/img/GitHub-Mark-Light-32px.png" alt="github logo">
+                &nbsp;
+                Github Login
+            </a>
+        </div>
+    </div>
+</body>
+
 </html>
