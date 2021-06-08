153f549259d7e1fcde7259ce1d8bbd6f6e492aa2
Abdulla Malik <abdulla.coder@gmail.com>
2021-06-09 02:34:42 +0600

    setup vue bootstrap

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,12 @@
 require('./bootstrap');
 require('./components');
+
 import Vue from 'vue';
+import { BootstrapVue, IconsPlugin, BVToastPlugin } from 'bootstrap-vue'
+
+Vue.use(BootstrapVue)
+Vue.use(IconsPlugin)
+Vue.use(BVToastPlugin)
 
 new Vue({
     el: '#root',
