959efb49cd1f66eafaca1254aac39d55625d13ef
Abdulla Malik <abdulla.coder@gmail.com>
2021-06-09 02:46:12 +0600

    update test

--- tests/Unit/GithubTest.php
+++ tests/Unit/GithubTest.php
@@ -10,18 +10,11 @@ class GithubTest extends TestCase
 {
 
     /** @test */
-    function it_contains_profile_info()
+    public function it_displayes_star_repos_for_the_user()
     {
         $github = $this->createMock(Github::class);
-        $github->method('profile')->willReturn('profile');
-        $this->assertEquals('profile', $github->profile());
-    }
-
-    /** @test */
-    function it_displayes_star_repos_for_the_user()
-    {
-        $github = $this->createMock(Github::class);
-        $github->method('starred')->willReturn('starred repos');
-        $this->assertEquals('starred repos', $github->starred());
+        $github->setAccessToken('some_token');
+        $github->method('getStarredRepos')->willReturn('starred repos');
+        $this->assertEquals('starred repos', $github->getStarredRepos());
     }
 }
