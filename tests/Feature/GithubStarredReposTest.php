f3f9cb8c48dbda18a09c95f9e7b4309d70e61dce
Abdulla Malik <abdulla.coder@gmail.com>
2021-06-09 03:58:56 +0600

    get starred repo test

---
+++ tests/Feature/GithubStarredReposTest.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Support\Facades\Http;
+
+class GithubStarredReposTest extends TestCase
+{
+    /** @test */
+    public function it_gets_starred_repos_from_github()
+    {
+        Http::fake(function ($request) {
+            return Http::response([
+                'starred_repos' => [
+                    ['repo1' => 'details'],
+                    ['repo2' => 'details'],
+                ]
+            ], 200);
+        });
+
+        $response = Http::post('/user/starred-repos', [
+            'token' => 'some_token',
+            'name' => 'abdulla malik',
+            'email' => 'abdulla.coder@gmail.com'
+        ]);
+        
+        $this->assertEquals([
+            'starred_repos' => [
+                    ['repo1' => 'details'],
+                    ['repo2' => 'details'],
+                ]
+            ], $response->body());
+    }
+}
