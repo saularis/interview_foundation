0b56d7cdf5c380b155737a184c873d3960917775
Abdulla Malik <abdulla.coder@gmail.com>
2021-06-09 02:36:02 +0600

    clean code

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,16 +1,18 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <title>Github Starred Repo</title>
-        <link rel="stylesheet" href="css/app.css">
-    </head>
-    <body>
-        <div id="root">
-            @yield('content')
-            <!-- <starred-repo></starred-repo> -->
-        </div>
-        <script src="/js/app.js"></script>
-    </body>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Github Starred Repo</title>
+    <link rel="stylesheet" href="css/app.css">
+</head>
+
+<body>
+    <div id="root">
+        @yield('content')
+    </div>
+    <script src="/js/app.js"></script>
+</body>
+
 </html>
